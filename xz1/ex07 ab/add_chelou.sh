5d7a2ed4207be7dc0997c55daac24dc7e4ce3e8c
Abdallahxz <161390040+Abdallahxz@users.noreply.github.com>
2024-03-20 16:32:42 +0100

    Delete xz1 directory

--- xz1/ex07 ab/add_chelou.sh
+++
@@ -1,3 +0,0 @@
-export FT_LINE1=7
-export FT_LINE2=15
-cat /etc/passwd | sed '/^#/d' | awk 'NR % 2 == 0' | cut -d ':' -f 1 | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' ', ' | sed 's/,/, /g' | sed 's/, $/./g' | tr -d '\n'
